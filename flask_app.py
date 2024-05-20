69eb2ebb8bf4bab60f1669b4ef54eedc6c9752f8
yun <yko2222@naver.com>
2024-05-21 06:13:08 +0900

    PORT

--- flask_app.py
+++ flask_app.py
@@ -95,4 +95,5 @@ def get_role_info(role, assigned_roles, player_name):
     return ""
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
